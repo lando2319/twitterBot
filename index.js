1c972a29e415a71b30d83d17b8b4fcba4218436d
Mike Land <mikepland@gmail.com>
2015-02-25 00:21:09 -0600

    Adds twitter npm

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var app = express()
+var FB = require('fb')
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
+
+FB.api('100004705442299', { fields: ['id', 'name'] }, function (res) {
+  if(!res || res.error) {
+    console.log(!res ? 'error occurred' : res.error);
+    return;
+  }
+  console.log(res.id);
+  console.log(res.name);
+}); 
+
 app.listen(3000)
 console.log("Running on localhost:3000")
